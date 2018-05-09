529c48ac9b096e3bd2a8f9cc816bacbdf9e648c2
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-09 15:28:48 +0200

    creating model files is now responsibility of PNMLToTapaal

--- fr.lip6.pnml.tapaal.runner/src/fr/pnml/tapaal/runner/VerifyWithProcess.java
+++ fr.lip6.pnml.tapaal.runner/src/fr/pnml/tapaal/runner/VerifyWithProcess.java
@@ -8,52 +8,35 @@ import java.nio.file.Files;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-import javax.swing.JFileChooser;
-
 import fr.lip6.move.gal.process.CommandLine;
 import fr.lip6.move.gal.process.Runner;
-import fr.lip6.move.pnml.ptnet.PetriNet;
 
 public class VerifyWithProcess {
+	
+	/**
+	 * Path to Tapaal's verifyPN binary executable.
+	 */
+    private String verifypath;
 
+	public VerifyWithProcess(String verifypath) {
+		this.verifypath = verifypath;
+	}
 
-    public List<String> orders;
-
-    // Constructor assigning a list of orders, calculated with a specified heuristic 
-    public VerifyWithProcess(List<String> orders) {
-        this.orders = orders;
-    }
-
-    
-
-    public void doVerify(String file_origin, String verifypath, String file_query) {
+	public void doVerify(String file_model, String file_query, List<String> orders) {
         // Creating the file names we need 
-        String[] tmp = file_origin.split("/");      //
-        String real_name = tmp[tmp.length-1];       // we get the name of the file only
-        String path_pnml = file_origin;
-
-        File file_tmp = null;
         try {// Creating a temporary file for the verifypn64 program
             /// temporary file is a .xml version of the .pnml chosen, for tapaal engine 
 
-            file_tmp = File.createTempFile(real_name.replace(".pnml",""), ".xml");
-            String file_model = file_tmp.getAbsolutePath();
-
             // Exporting the file path_pnml to file_model using our Parser/Writer PNMLToTAPN for tapaal
-            PNMLToTAPN exporter = new PNMLToTAPN(path_pnml,file_model,null);
-            exporter.toTAPN();
 
             // defining query file path and options            
             String options = "-k 0 -s BestFS -r 1 -q 0 -ctl czero -x 1";
-
             String arguments = options+" "+file_model+" "+file_query;
-
             String commandLine = verifypath+" "+arguments;
-            String[] commands = commandLine.split(" ");
 
             // creating command line for the runner
             CommandLine cl = new CommandLine();
-            for(String str : commands) {
+            for(String str : commandLine.split(" ")) {
                 cl.addArg(str);
             }
             File tempo_file =  File.createTempFile("temporary",".result");           
@@ -74,80 +57,75 @@ public class VerifyWithProcess {
             long usedMB = (rt.totalMemory() - rt.freeMemory()) / 1024 / 1024;
             String str_tmp ="Memory usage : "+usedMB+"Mb";
             System.out.println(str_tmp);            
-            File tmp_file = new File(file_origin);
-            printACSV(usedMB,System.currentTimeMillis()- timestamp, tmp_file.getParent());
+            printACSV(usedMB,System.currentTimeMillis()- timestamp, new File(file_model).getParent());
         } catch (IOException | TimeoutException | InterruptedException e) {
             e.printStackTrace();
-        }finally {
-            if(null!=file_tmp) { // If the program run without error, delete the temporary file before exiting
-                file_tmp.delete();
-            }
         }
-    }
+	}
     
-    public void doVerify(PetriNet net, String verifypath, String file_query) {
-        // Creating the file names we need 
-       
-
-        File file_tmp = null;
-        try {// Creating a temporary file for the verifypn64 program
-            /// temporary file is a .xml version of the .pnml chosen, for tapaal engine 
-
-            file_tmp = File.createTempFile(net.getName().getText(), ".xml");
-            String file_model = file_tmp.getAbsolutePath();
-
-            // Exporting the file path_pnml to file_model using our Parser/Writer PNMLToTAPN for tapaal
-            PNMLToTAPN exporter = new PNMLToTAPN(net,file_model,null);
-            exporter.toTAPN();
-
-            // defining query file path and options            
-            String options = "-k 0 -s BestFS -r 1 -q 0 -ctl czero -x 1";
-
-            String arguments = options+" "+file_model+" "+file_query;
-
-            String commandLine = verifypath+" "+arguments;
-            String[] commands = commandLine.split(" ");
-
-            // creating command line for the runner
-            CommandLine cl = new CommandLine();
-            for(String str : commands) {
-                cl.addArg(str);
-            }
-            File tempo_file =  File.createTempFile("temporary",".result");           
-
-            long timeout = 300000;
-            
-            boolean errToOut = false;
-            double timestamp = System.currentTimeMillis();
-            System.out.println("Launching runner ...");
-            
-            if(Runner.runTool(timeout, cl, tempo_file, true)!=0){
-            	System.err.println("Error while running the tool\n");
-            	if(null!=file_tmp) { // If the program run without error, delete the temporary file before exiting
-                    file_tmp.delete();
-                }
-            	return;
-            }
-            
-            Files.lines(tempo_file.toPath()).forEach(line -> System.out.println(line));
-            
-            // displaying the memory consumption of the runtime environment
-            System.gc();
-            Runtime rt = Runtime.getRuntime();
-            long usedMB = (rt.totalMemory() - rt.freeMemory()) / 1024 / 1024;
-            String str_tmp ="Memory usage : "+usedMB+"Mb";
-            System.out.println(str_tmp);            
-
-            printACSV(usedMB,System.currentTimeMillis()- timestamp, net.getName().getText());
-            
-        } catch (IOException | TimeoutException | InterruptedException e) {
-            e.printStackTrace();
-        }finally {
-            if(null!=file_tmp) { // If the program run without error, delete the temporary file before exiting
-                file_tmp.delete();
-            }
-        }
-    }
+//    public void doVerify(PetriNet net, String verifypath, String file_query) {
+//        // Creating the file names we need 
+//       
+//
+//        File file_tmp = null;
+//        try {// Creating a temporary file for the verifypn64 program
+//            /// temporary file is a .xml version of the .pnml chosen, for tapaal engine 
+//
+//            file_tmp = File.createTempFile(net.getName().getText(), ".xml");
+//            String file_model = file_tmp.getAbsolutePath();
+//
+//            // Exporting the file path_pnml to file_model using our Parser/Writer PNMLToTAPN for tapaal
+//            PNMLToTAPN exporter = new PNMLToTAPN(net,file_model,null);
+//            exporter.toTAPN();
+//
+//            // defining query file path and options            
+//            String options = "-k 0 -s BestFS -r 1 -q 0 -ctl czero -x 1";
+//
+//            String arguments = options+" "+file_model+" "+file_query;
+//
+//            String commandLine = verifypath+" "+arguments;
+//            String[] commands = commandLine.split(" ");
+//
+//            // creating command line for the runner
+//            CommandLine cl = new CommandLine();
+//            for(String str : commands) {
+//                cl.addArg(str);
+//            }
+//            File tempo_file =  File.createTempFile("temporary",".result");           
+//
+//            long timeout = 300000;
+//            
+//            boolean errToOut = false;
+//            double timestamp = System.currentTimeMillis();
+//            System.out.println("Launching runner ...");
+//            
+//            if(Runner.runTool(timeout, cl, tempo_file, true)!=0){
+//            	System.err.println("Error while running the tool\n");
+//            	if(null!=file_tmp) { // If the program run without error, delete the temporary file before exiting
+//                    file_tmp.delete();
+//                }
+//            	return;
+//            }
+//            
+//            Files.lines(tempo_file.toPath()).forEach(line -> System.out.println(line));
+//            
+//            // displaying the memory consumption of the runtime environment
+//            System.gc();
+//            Runtime rt = Runtime.getRuntime();
+//            long usedMB = (rt.totalMemory() - rt.freeMemory()) / 1024 / 1024;
+//            String str_tmp ="Memory usage : "+usedMB+"Mb";
+//            System.out.println(str_tmp);            
+//
+//            printACSV(usedMB,System.currentTimeMillis()- timestamp, net.getName().getText());
+//            
+//        } catch (IOException | TimeoutException | InterruptedException e) {
+//            e.printStackTrace();
+//        }finally {
+//            if(null!=file_tmp) { // If the program run without error, delete the temporary file before exiting
+//                file_tmp.delete();
+//            }
+//        }
+//    }
     
     
     
