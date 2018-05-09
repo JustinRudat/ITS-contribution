aaefb372479195717af10171a2f944cfd131acbd
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-09 16:30:49 +0200

    a bool to interrupt treatment if problem solved structurally

--- fr.lip6.pnml.tapaal.application/src/fr/lip6/pnml/tapaal/application/PNMLToTapaalTransformer.java
+++ fr.lip6.pnml.tapaal.application/src/fr/lip6/pnml/tapaal/application/PNMLToTapaalTransformer.java
@@ -10,10 +10,11 @@ public interface PNMLToTapaalTransformer {
 	 * If load was successful, create Tapaal format files for both Petri net and queries.
 	 * @param modelPath absolute path to PNML file
 	 * @param formulaPath absolute path to Model Checking Contest format XML property file	
+	 * @return true if the problem is now solved, false if we need to verify more.
 	 * @exception UnsupportedOp if a COL Petri net is passed. 
 	 * @exception IOEx if problems with work folder write
 	 */
-	public void loadTransformPNML(String modelPath, String formulaPath) throws IOException;
+	public boolean loadTransformPNML(String modelPath, String formulaPath) throws IOException;
 	/** 
 	 * Path to a Tapaal format net representing the original net.
 	 * @return a path to a file, in current work folder.
