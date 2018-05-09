d52d7aa286f765d5f0ec6bbe38f171daae09c8f0
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-09 16:31:22 +0200

    patch inputArc vs outputArc + refactor slightly

--- fr.lip6.pnml.tapaal.application/src/fr/lip6/pnml/tapaal/application/TapaalBuilder.java
+++ fr.lip6.pnml.tapaal.application/src/fr/lip6/pnml/tapaal/application/TapaalBuilder.java
@@ -1,79 +1,63 @@
 package fr.lip6.pnml.tapaal.application;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.Writer;
-import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.List;
 
 import android.util.SparseIntArray;
-import fr.lip6.move.gal.nupn.PTNetReader;
 import fr.lip6.move.gal.util.MatrixCol;
-import fr.lip6.move.pnml.ptnet.PetriNet;
-import fr.lip6.move.pnml.ptnet.Place;
-import fr.lip6.move.pnml.ptnet.impl.PetriNetImpl;
 
 public class TapaalBuilder {
 
 	public static File buildTapaal(MatrixCol flowPT, MatrixCol flowTP, List<String> pnames, List<String> tnames,
-			List<Integer> marks,String pt_name) {
-	    PetriNet retour = null;
-	    File xml_ptnet=null;
-	    try {
-	        List<SparseIntArray> col_pt = flowPT.getColumns();
-	        List<SparseIntArray> col_tp = flowTP.getColumns();
-	    
-	    
-            xml_ptnet = new File(pt_name+".pnml");
-            if(!xml_ptnet.exists()) {
-                xml_ptnet.createNewFile();
-            }
-            ArrayList<Place> places = new ArrayList<Place>();
-            PrintWriter pw = new PrintWriter(xml_ptnet);
-            pw.print("<pnml>\n" + 
-                    "<net id=\""+pt_name+"\" type=\"P/T net\">\n");            
-            for(String pname : pnames) {
-                int i = pnames.indexOf(pname);
-                Integer marking = marks.get(i);
-                pw.print("<place id=\""+pname+"\" name=\""+pname+"\" invariant=\"&lt; inf\" initialMarking=\""+marking.intValue()+"\" />\n");
-                
-            }
-            
-            for(String transition : tnames) {
-                pw.print("<transition id=\""+transition+"\" name=\""+transition+"\" urgent=\"false\"/>\n");
-            }
-            
-            for(SparseIntArray sparseArray : col_tp) {
-                for(int i=0;i<sparseArray.size();i++) {
-                    if(sparseArray.get(i)!=0) { // ou >=1
-                        int index = col_tp.indexOf(sparseArray);
-                        pw.print("<inputArc source=\""+tnames.get(index)+"\" target=\""+pnames.get(i)+"\"><inscription><value>"+sparseArray.get(i)+"</value></inscription></inputArc>\n");
-                    }
-                }
-            }
-            for(SparseIntArray sparseArray : col_pt) {
-                for(int i=0;i<sparseArray.size();i++) {
-                    if(sparseArray.get(i)!=0) {
-                        int index = col_pt.indexOf(sparseArray);
-                        pw.print("<outputArc source=\""+pnames.get(i)+"\" target=\""+tnames.get(index)+"\"><inscription><value>"+sparseArray.get(i)+"</value></inscription></outputArc>\n");
-                    }
-                }
-            }
-            pw.print("</net>\n" + 
-                    "</pnml>");
-            pw.flush();
-            PTNetReader ptreader = new PTNetReader(); 
-            retour = ptreader.loadFromXML(new BufferedInputStream(new FileInputStream(xml_ptnet)));
-            
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }   
-	    
+			List<Integer> marks,String pt_name) throws IOException {
+		List<SparseIntArray> col_pt = flowPT.getColumns();
+		List<SparseIntArray> col_tp = flowTP.getColumns();
+
+
+		File xml_ptnet = new File(pt_name);
+		PrintWriter pw = new PrintWriter(xml_ptnet);
+		pw.print("<pnml>\n" + 
+				"<net id=\""+pt_name+"\" type=\"P/T net\">\n");            
+		
+		int pi = 0;
+		for(String pname : pnames) {
+			Integer marking = marks.get(pi);
+			pw.print("<place id=\""+pname+"\" name=\""+pname+"\" invariant=\"&lt; inf\" initialMarking=\""+marking.intValue()+"\" />\n");
+			pi++;
+		}
+
+		for(String transition : tnames) {
+			pw.print("<transition id=\""+transition+"\" name=\""+transition+"\" urgent=\"false\"/>\n");
+		}
+
+		for (int tindex = 0 ; tindex  < col_pt.size() ; tindex++) {
+			SparseIntArray sparseArray = col_pt.get(tindex);
+			for(int i=0;i<sparseArray.size();i++) {
+				
+				int pindex = sparseArray.keyAt(i);
+				int tokens = sparseArray.valueAt(i);
+				
+				pw.print("<inputArc source=\""+pnames.get(pindex)+"\" target=\""+tnames.get(tindex)+"\"><inscription><value>"+tokens+"</value></inscription></inputArc>\n");
+			}			
+		}
+		for (int tindex = 0 ; tindex  < col_tp.size() ; tindex++) {
+			SparseIntArray sparseArray = col_tp.get(tindex);
+			for(int i=0;i<sparseArray.size();i++) {
+				
+				int pindex = sparseArray.keyAt(i);
+				int tokens = sparseArray.valueAt(i);
+				
+				pw.print("<outputArc source=\""+tnames.get(tindex)+"\" target=\""+pnames.get(pindex)+"\"><inscription><value>"+tokens+"</value></inscription></inputArc>\n");
+			}			
+		}
+		pw.print("</net>\n" + 
+				"</pnml>");
+		pw.flush();
+		pw.close();
+
+
 		return xml_ptnet;
 	}
 
