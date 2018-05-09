832ca4cfd57bb03401158100fe8d5c25c6ce2cf3
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-09 15:28:08 +0200

    A raw path : translate PNML (direct) + leave formula alone

---
+++ fr.lip6.pnml.tapaal.application/src/fr/lip6/pnml/tapaal/application/BasicTapaalTransformer.java
@@ -0,0 +1,39 @@
+package fr.lip6.pnml.tapaal.application;
+
+import java.io.IOException;
+
+import fr.pnml.tapaal.runner.PNMLToTAPN;
+
+public class BasicTapaalTransformer implements PNMLToTapaalTransformer {
+
+	private String cwd;
+	private String formulaPath;
+
+	@Override
+	public void setWorkFolder(String path) {
+		this.cwd = path;
+	}
+
+	@Override
+	public void loadTransformPNML(String modelPath, String formulaPath)  throws IOException {
+        // Exporting the file path_pnml to file_model using our Parser/Writer PNMLToTAPN for tapaal		
+        PNMLToTAPN exporter = new PNMLToTAPN(modelPath,cwd+"/model.pnml",null);       
+        exporter.toTAPN();
+        // Queries are not translated in this version !
+        this.formulaPath = formulaPath;
+	}
+
+
+	@Override
+	public String getPathToTapaalNet() {
+		return cwd+"/model.pnml";
+	}
+
+	@Override
+	public String getPathToTapaalQuery() {
+		return formulaPath;
+	}
+
+
+
+}
