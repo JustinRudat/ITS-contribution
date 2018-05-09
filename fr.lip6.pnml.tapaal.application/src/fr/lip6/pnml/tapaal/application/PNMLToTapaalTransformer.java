4418346857ae3dad60f20aef4e61d138da0fd99e
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-09 15:27:35 +0200

    an interface to put common parts of PNML->GAL->Tapaal and "raw" path

---
+++ fr.lip6.pnml.tapaal.application/src/fr/lip6/pnml/tapaal/application/PNMLToTapaalTransformer.java
@@ -0,0 +1,27 @@
+package fr.lip6.pnml.tapaal.application;
+
+import java.io.IOException;
+
+public interface PNMLToTapaalTransformer {
+	/** Folder where output files will be produced. */
+	public void setWorkFolder(String path);
+	/**
+	 * Load (parse) the input PNML file, which MUST be P/T not COL.
+	 * If load was successful, create Tapaal format files for both Petri net and queries.
+	 * @param modelPath absolute path to PNML file
+	 * @param formulaPath absolute path to Model Checking Contest format XML property file	
+	 * @exception UnsupportedOp if a COL Petri net is passed. 
+	 * @exception IOEx if problems with work folder write
+	 */
+	public void loadTransformPNML(String modelPath, String formulaPath) throws IOException;
+	/** 
+	 * Path to a Tapaal format net representing the original net.
+	 * @return a path to a file, in current work folder.
+	 */
+	public String getPathToTapaalNet();
+	/** 
+	 * Path to a Tapaal/PNMCC format net representing the queries.
+	 * @return a path to a file, in current work folder.
+	 */
+	public String getPathToTapaalQuery();
+}
